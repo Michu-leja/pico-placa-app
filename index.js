affa6ddbfc91009c38a93e0627661f75e9c98f71
Michelle Peralbo <michelle14@hotmail.com>
2020-05-16 18:04:03 -0500

    [Michelle] - Add index.pug for UI

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || "3000";
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
-    res.status(200).send("Testing node");
+    res.render("index");
   });
 
   app.listen(port, () => {
