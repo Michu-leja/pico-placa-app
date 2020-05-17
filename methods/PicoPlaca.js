6cf030cb01bae98d823b1d2d2886afe006d05b46
Michelle Peralbo <michelle14@hotmail.com>
2020-05-16 21:46:03 -0500

    [Michelle] - added methods to check the hours and  to know if a user can road and the unit tests relates

--- methods/PicoPlaca.js
+++ methods/PicoPlaca.js
@@ -1,15 +1,49 @@
 
 module.exports = {
     getDayOfDate: (stringDate) => {
-
         splitDate = stringDate.split("-")
         year = splitDate[0]
         month = (splitDate[1].includes("0")) ? splitDate[1].replace("0", "") : splitDate[1]
         month = month - 1  //month from 0-11
         day = (splitDate[2].includes("0")) ? splitDate[2].replace("0", "") : splitDate[2]
-        var convertedDate = new Date(year, month, day)
-        var dayOfWeek = convertedDate.getDay()
+        let convertedDate = new Date(year, month, day)
+        let dayOfWeek = convertedDate.getDay()
         return dayOfWeek
 
+    },
+
+    getLastDigitOfLicensePlate: (licensePlate) => {
+        let lastDigit = licensePlate.slice(-1)
+        return lastDigit
+    },
+
+    isHourInTheThreshold: (hour) => {
+        if ((hour < "9:30" && hour > "7:00") || (hour > "16:00" && hour < "19:30")) {
+            return true
+        } else {
+            return false
+        }
+    },
+
+    isAllowedToRoad: (dayOfWeek, lastDayLicenseNumber, hour) => {
+        if (dayOfWeek === 0 || dayOfWeek === 6) {
+            return true
+        }
+        if (dayOfWeek === 1 && lastDayLicenseNumber === 1 || lastDayLicenseNumber === 2) {
+            return this.isHourInTheThreshold(hour)
+        }
+        if(dayOfWeek === 2 && lastDayLicenseNumber === 3 || lastDayLicenseNumber === 4){
+            return this.isHourInTheThreshold(hour)
+        }
+        if(dayOfWeek === 3 && lastDayLicenseNumber === 5 || lastDayLicenseNumber === 6){
+            return this.isHourInTheThreshold(hour)
+        }
+        if(dayOfWeek === 4 && lastDayLicenseNumber === 7 || lastDayLicenseNumber === 8){
+            return this.isHourInTheThreshold(hour)
+        }
+        if(dayOfWeek === 5 && lastDayLicenseNumber === 9 || lastDayLicenseNumber === 0){
+            return this.isHourInTheThreshold(hour)
+        }
     }
+
 }
--- test/PicoPlacaTest.js
+++ test/PicoPlacaTest.js
@@ -1,18 +1,81 @@
 const mocha = require('mocha')
 const assert = require('assert')
-const sinon = require('sinon')
 const picoPlaca = require("../methods/PicoPlaca.js")
 
 
-describe('Test of PIco y placa functions', function () {
+describe('Test of Pico y placa functions', function () {
 
-    it('should return day of the week of a given date', function () {
+    it('should return day of the week as an int of a given date', function () {
         inputDate = "2020-05-17"
         dayExpected = 0 //sunday
 
         resultDay = picoPlaca.getDayOfDate(inputDate)
 
         assert.equal(resultDay, dayExpected)
+    }),
+
+    it('should return the last digit =0 of the license plate', function () {
+        inputLicensePlate = "POC-1010"
+        digitExpected = 0 
+
+        resultDigit = picoPlaca.getLastDigitOfLicensePlate(inputLicensePlate)
+
+        assert.equal(resultDigit, digitExpected)
+    }),
+    it('should return the last digit = 9 of the license plate', function () {
+        inputLicensePlate = "POC-1089"
+        digitExpected = 9 
+
+        resultDigit = picoPlaca.getLastDigitOfLicensePlate(inputLicensePlate)
+
+        assert.equal(resultDigit, digitExpected)
+    }),
+
+ it('should return true if the hours are  in the threshold morning', function () {
+        inputHour = "7:35"
+        valueExpected =  true
+
+        resultDigit = picoPlaca.isHourInTheThreshold(inputHour)
+
+        assert.equal(resultDigit, valueExpected)
+    }),
+
+    it('should return true if the hours are  in the threshold afternoon', function () {
+        inputHour = "18:35"
+        valueExpected =  true
+
+        resultDigit = picoPlaca.isHourInTheThreshold(inputHour)
+
+        assert.equal(resultDigit, valueExpected)
+    }),
+
+    it('should return false if the hours are  in the threshold', function () {
+        inputHour = "6:50"
+        valueExpected =  false
+
+        resultDigit = picoPlaca.isHourInTheThreshold(inputHour)
+
+        assert.equal(resultDigit, valueExpected)
+    }),
+
+    it('should return false if the hours are  in the threshold afternoon', function () {
+        inputHour = "21:30"
+        valueExpected =  false
+
+        resultDigit = picoPlaca.isHourInTheThreshold(inputHour)
+
+        assert.equal(resultDigit, valueExpected)
+    }),
+
+    it('should return true if is allowed to road day 0 = sunday , last digit =9 hour 20:20', function () {
+        inputDay= 0
+        inputLastDigit =9
+        inputHour = "20:20"
+        valueExpected =  true
+
+        resultDigit = picoPlaca.isAllowedToRoad(inputDay, inputLastDigit, inputHour)
+
+        assert.equal(resultDigit, valueExpected)
     })
 
 })
